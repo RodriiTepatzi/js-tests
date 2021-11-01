b1004b51ea4dc75f5efdd4dbfe6bc2f690a2ee34
RodriiTepatzi <rodrif19@hotmail.com>
2021-11-01 09:50:27 -0600

    Tipos de datos

---
+++ TiposDeDatos.js
@@ -0,0 +1,35 @@
+// Tipo number
+var entero = 27;
+console.log(entero);
+var flotante = 27.7;
+console.log(entero);
+
+// Tipo string
+var string = "Rodrigo";
+console.log(string);
+
+// Boleanos
+
+var booleano = true;
+console.log(booleano);
+
+// Tipo indefinido
+var indefinido = undefined;
+console.log(indefinido);
+
+// Tipo nulo
+var nulo = null;
+console.log(nulo);
+
+/* 
+    TIpos estructurales
+*/
+
+// Objeto
+
+var objeto = { nombre: "Rodrigo", apellido: "Tepatzi" };
+console.log(objeto);
+// Lista
+
+var arreglo = ["Mexico", "Ecuador", "Venezuela"];
+console.log(arreglo);
